e9ca282f6aa35c50438bc6370763df5b6af2f48c
Felix Sebastian <hi@felixsebastian.dev>
2022-06-29 06:32:00 +0000

    added express app

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello!")
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (_, res) => {
+  let x = 3
+  console.log(x);
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
